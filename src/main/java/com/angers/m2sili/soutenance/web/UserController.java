b1cf44c311c917c794e73d1f6d51f437ce43e324
killzoner <twinny902000@yahoo.fr>
2014-05-27 13:51:39 +0200

    clean warning

--- src/main/java/com/angers/m2sili/soutenance/web/UserController.java
+++ src/main/java/com/angers/m2sili/soutenance/web/UserController.java
@@ -1,8 +1,6 @@
 package com.angers.m2sili.soutenance.web;
 import java.util.List;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
