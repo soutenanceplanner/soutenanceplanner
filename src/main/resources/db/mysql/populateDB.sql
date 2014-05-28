f6b0279c5ea92d93893abd6c9b6d02470bac5a3e
Benoît Caufriez <benoit.caufriez@gmail.com>
2014-05-28 15:30:51 +0200

    Fix error email -> mail and add users

--- src/main/resources/db/mysql/populateDB.sql
+++ src/main/resources/db/mysql/populateDB.sql
@@ -2,8 +2,12 @@ INSERT IGNORE INTO user VALUES (1, 0, 'dd', 'dd@dd.fr', 'password');
 INSERT IGNORE INTO user VALUES (2, 0, 'admin', 'admin@soutenance.fr', 'password');
 INSERT IGNORE INTO user VALUES (3, 0, 'pevers', 'everspierre@gmail.com', 'pevers');
 INSERT IGNORE INTO user VALUES (4, 0, 'bcaufriez', 'benoit.caufriez@gmail.com', 'bcaufriez');
-INSERT IGNORE INTO user VALUES (4, 0, 'jgrimonprez', 'jean.grimonprez@etud.univ-angers.fr', 'jgrimonprez');
+INSERT IGNORE INTO user VALUES (5, 0, 'jgrimonprez', 'jean.grimonprez@etud.univ-angers.fr', 'jgrimonprez');
 INSERT IGNORE INTO user VALUES (6, 0, 'mdesmauts', 'maxime.desmauts@gmail.com', 'mdesmauts');
+INSERT IGNORE INTO user VALUES (7, 1, 'admin1', 'admin1@soutenance.fr', 'admin1');
+INSERT IGNORE INTO user VALUES (8, 1, 'admin2', 'admin2@soutenance.fr', 'admin2');
+INSERT IGNORE INTO user VALUES (9, 2, 'user1', 'user1@soutenance.fr', 'user1');
+INSERT IGNORE INTO user VALUES (10, 2, 'user2', 'user2@soutenance.fr', 'user2');
 
 INSERT IGNORE INTO formation VALUES (1, 'L3 Info');
 INSERT IGNORE INTO formation VALUES (2, 'M1 Info');
