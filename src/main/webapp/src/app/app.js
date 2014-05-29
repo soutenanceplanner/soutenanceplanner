f8041d0208249ccba4ad6d02c979260919e61377
killzoner <twinny902000@yahoo.fr>
2014-05-29 16:09:51 +0200

    Clean

--- src/main/webapp/src/app/app.js
+++ src/main/webapp/src/app/app.js
@@ -1,80 +1,74 @@
 /*global angular */
 /*jshint unused:false */
 
-angular.module('soutenanceplanner', [ 'templates-app', 'templates-common',
-		'ui.router', 'ngAnimate', 'ui.calendar',
+angular.module('soutenanceplanner', [ 
+		'templates-app',
+		'templates-common',
+		'ui.router',
+		'ngAnimate',
+		'ui.calendar',
 
 		// dans cet ordre là pour éviter conflits
-		'ui.bootstrap', 'mgcrea.ngStrap', 'angularFileUpload', 'services.i18n',
+		'ui.bootstrap',
+		'mgcrea.ngStrap', 
+
+		'angularFileUpload',
+		'services.i18n',
 
 		// Commons
 		'soutenanceplanner.factory', 'soutenanceplanner.enum',
 
 		// modules
-		'soutenanceplanner.home', 'soutenanceplanner.calendar',
-		'soutenanceplanner.login', 'soutenanceplanner.account',
-		'soutenanceplanner.formation' ]);
+		'soutenanceplanner.home',
+		'soutenanceplanner.calendar',
+		'soutenanceplanner.login', 
+		'soutenanceplanner.account',
+		'soutenanceplanner.formation' 
+]);
 
-angular
-		.module('soutenanceplanner')
+angular.module('soutenanceplanner')
 
-		// INTEG EXTERNE
-		// .value('WS_SERVER_URL',
-		// 'http://soutenanceplanner.m2sili.cloudbees.net')
+// INTEG EXTERNE
+//.value('WS_SERVER_URL','http://soutenanceplanner.m2sili.cloudbees.net')
 
-		// LOCALHOST
-		.value('WS_SERVER_URL', 'http://localhost:8082/soutenanceplanner')
+// LOCALHOST
+.value('WS_SERVER_URL', 'http://localhost:8082/soutenanceplanner')
 
-		.config([ '$urlRouterProvider', function($urlRouterProvider) {
-			$urlRouterProvider.otherwise('/');
-		} ])
+.config([ '$urlRouterProvider', function($urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+} ])
 
-		.config([ '$logProvider', function($logProvider) {
-			$logProvider.debugEnabled(true);
-		} ])
+.config([ '$logProvider', function($logProvider) {
+	$logProvider.debugEnabled(true);
+} ])
 
-		.config([ '$locationProvider', function($locationProvider) {
-			$locationProvider.html5Mode(false);
-		} ])
+.config([ '$locationProvider', function($locationProvider) {
+	$locationProvider.html5Mode(false);
+} ])
 
-		// cross origin => TO DELETE
-		.config([ '$httpProvider', function($httpProvider) {
-			$httpProvider.defaults.withCredentials = true;
-			// $httpProvider.defaults.useXDomain = true;
-			// delete $httpProvider.defaults.headers.common['X-Requested-With'];
-		} ])
+// cross origin => TO DELETE
+.config([ '$httpProvider', function($httpProvider) {
+	$httpProvider.defaults.withCredentials = true;
+	// $httpProvider.defaults.useXDomain = true;
+	// delete $httpProvider.defaults.headers.common['X-Requested-With'];
+} ])
 
-		.run(
-				[
-						'$rootScope',
-						'$state',
-						'$stateParams',
-						'$http',
-						function($rootScope, $state, $stateParams, $http) {
-							$http.defaults.headers.contentType = "application/json; charset=utf-8";
-						} ])
+.run(['$rootScope','$state','$stateParams','$http',
+	function($rootScope, $state, $stateParams, $http) {
+		$http.defaults.headers.contentType = "application/json; charset=utf-8";
+} ])
 
-		.controller(
-				'MainCtrl',
-				[
-						'$scope',
-						'$rootScope',
-						'$location',
-						'$log',
-						'$state',
-						'i18n',
-						function($scope, $rootScope, $location, $log, $state,
-								i18n) {
-							$log.debug("MainCtrl");
+.controller('MainCtrl',['$scope','$rootScope','$location','$log','$state','i18n',
+	function($scope, $rootScope, $location, $log, $state, i18n) {
+		$log.debug("MainCtrl");
 
-							$scope.init = function() {
+		$scope.init = function() {
+			// i18n
+			$rootScope.i18n = i18n;
+		};
 
-								// i18n
-								$rootScope.i18n = i18n;
-							};
-
-							$scope.init();
-						} ])
+		$scope.init();
+} ])
 
 
 .controller('MenuCtrl', ['$rootScope', '$scope', '$log','$state', '$location',
