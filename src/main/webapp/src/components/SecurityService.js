cc9d821ed0c4d6e60ed24e10489a11923c706b1a
killzoner <twinny902000@yahoo.fr>
2014-05-30 16:25:25 +0200

    Add file Secu Service

---
+++ src/main/webapp/src/components/SecurityService.js
@@ -0,0 +1,31 @@
+angular.module('soutenanceplanner.security', []);
+
+angular.module('soutenanceplanner.security')
+
+.service('SecurityService', [ '$http', 'WS_SERVER_URL',
+
+	function($http, WS_SERVER_URL) {
+
+		var SecurityService = {
+
+			retrieve: function(){
+				return $http({
+					method: 'GET',
+					url: WS_SERVER_URL + "/security/retrieve",
+					data: {}
+				});
+			},
+
+			authenticate: function(){
+				return $http({
+					method: 'POST',
+					url: WS_SERVER_URL + "/security/authenticate",
+					data: {}
+				});
+			}
+
+		};
+
+		return SecurityService;
+	}
+]);
