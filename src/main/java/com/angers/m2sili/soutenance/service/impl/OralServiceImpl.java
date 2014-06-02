5b3f6820d3d0380d0513d826b1c2625e811def3b
killzoner <twinny902000@yahoo.fr>
2014-06-02 19:24:42 +0200

    clean warnings

--- src/main/java/com/angers/m2sili/soutenance/service/impl/OralServiceImpl.java
+++ src/main/java/com/angers/m2sili/soutenance/service/impl/OralServiceImpl.java
@@ -3,7 +3,6 @@ package com.angers.m2sili.soutenance.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
