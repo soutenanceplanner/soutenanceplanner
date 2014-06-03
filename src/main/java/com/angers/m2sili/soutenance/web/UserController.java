8ee49035cc17b665bcc947d9a7b1f91b4edbb2e1
maxime-desmauts <maxime.desmauts@gmail.com>
2014-06-03 15:30:33 +0200

    Réglage conflit

--- src/main/java/com/angers/m2sili/soutenance/web/UserController.java
+++ src/main/java/com/angers/m2sili/soutenance/web/UserController.java
@@ -1,4 +1,10 @@
 package com.angers.m2sili.soutenance.web;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +18,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.angers.m2sili.soutenance.model.User;
 import com.angers.m2sili.soutenance.service.UserService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Controller de User.
@@ -21,43 +29,61 @@ import com.angers.m2sili.soutenance.service.UserService;
  */
 
 @Controller
-//@PreAuthorize("isAuthenticated()")
+// @PreAuthorize("isAuthenticated()")
 @RequestMapping(value = "/user")
 public class UserController extends BaseController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping(value = "/new", method = RequestMethod.POST)
 	public @ResponseBody
-	User create(@RequestBody User user) {
-		User newUser = userService.create(user);
-		return newUser;
+	User create(@RequestBody String user) throws UnsupportedEncodingException,
+			IOException {
+		try (Reader reader = new InputStreamReader(new ByteArrayInputStream(
+				user.getBytes()), "UTF-8")) {
+			Gson gson = new GsonBuilder().create();
+			User p = gson.fromJson(reader, User.class);
+			System.out.println(p);
+
+			User newUser = userService.create(p);
+			return newUser;
+		}
+		// User newUser = userService.create(user);
+		// return new User();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public @ResponseBody
 	void delete(@PathVariable Integer id) {
 		userService.delete(id);
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public @ResponseBody
 	User get(@PathVariable Integer id) {
 		return userService.get(id);
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public @ResponseBody
 	List<User> list() {
 		return userService.getAll();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public @ResponseBody
-	User update(@RequestBody User user) {
-		return userService.update(user);
+	User update(@RequestBody String user) throws UnsupportedEncodingException, IOException {
+		try (Reader reader = new InputStreamReader(new ByteArrayInputStream(
+				user.getBytes()), "UTF-8")) {
+			Gson gson = new GsonBuilder().create();
+			User p = gson.fromJson(reader, User.class);
+			System.out.println(p);
+
+			User newUser = userService.update(p);
+			return newUser;
+		}
 	}
-	
+
 }
