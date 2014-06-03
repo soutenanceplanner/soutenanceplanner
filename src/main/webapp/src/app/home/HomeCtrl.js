b7fd8bfbfa21f38de033d745605b3fca21c0dd0d
killzoner <twinny902000@yahoo.fr>
2014-06-03 19:20:17 +0200

    Clean

--- src/main/webapp/src/app/home/HomeCtrl.js
+++ src/main/webapp/src/app/home/HomeCtrl.js
@@ -1,22 +1,11 @@
-angular
-		.module('soutenanceplanner.home')
+angular.module('soutenanceplanner.home')
 
-		.controller(
-				'HomeCtrl',
-				[
-						'$scope',
-						'$log',
-						'AccountService',
-						'HomeService',
-						'$translate',
-						'dialogs',
-						function($scope, $log, AccountService, HomeService,$translate,dialogs) {	
-							$log.debug('HomeCtrl');			
+.controller('HomeCtrl',['$scope','$log','AccountService','HomeService','$translate','dialogs',
+	function($scope, $log, AccountService, HomeService,$translate,dialogs) {	
+		$log.debug('HomeCtrl');			
 
+		$scope.open = function () {
+			dialogs.error("Message d'erreur !!!","contenu du message !!");
+		};
 
-$scope.open = function () {
-	dialogs.error("Message d'erreur !!!","contenu du message !!");
-};
-						
-
-						} ]);
+} ]);
