410bb8155467fafd534654160c269b0ffec9777e
killzoner <twinny902000@yahoo.fr>
2014-06-03 18:26:10 +0200

    clean

--- src/main/webapp/src/app/account/AccountService.js
+++ src/main/webapp/src/app/account/AccountService.js
@@ -1,8 +1,8 @@
 angular.module('soutenanceplanner.account')
 
-.service('AccountService', [ '$http', 'WS_SERVER_URL','transformRequestAsFormPost',
+.service('AccountService', [ '$http', 'WS_SERVER_URL',
 
-	function($http, WS_SERVER_URL, transformRequestAsFormPost) {
+	function($http, WS_SERVER_URL) {
 
 		var AccountService = {
 
